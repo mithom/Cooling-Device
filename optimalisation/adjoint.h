4d42e5ea7b1e8ad270ac07ee411875a9209aadce
Thomas Michiels <mi_thom@hotmail.com>
2017-03-23 15:43:39 +0100

    edges adjoint

--- optimalisation/adjoint.h
+++ optimalisation/adjoint.h
@@ -55,7 +55,66 @@ public:
                 dydp[i*n+j] = test2;
             }
         }
-        //TODO: edge cases
+        for(int i=1;i<n-1;i++){//left edge
+            dAdpi.setZero();
+
+            dAdpi.insert(n*i,(i)*n) = 3;
+            dAdpi.insert(i*n,(i-1)*n) = -1;
+            dAdpi.insert(i*n,(i+1)*n) = -1;
+            dAdpi.insert(i*n,(i)*n+1) = -1;
+
+            dAdpi.insert((i+1)*n,(i)*n) = 1;
+            dAdpi.insert((i-1)*n,(i)*n) = 1;
+            dAdpi.insert(i*n+1,(i)*n) = 1;
+            RowVectorXd dydpi = d * A_inv;
+            double test2 = dydpi * dAdpi * x;
+            dydp[i*n] = test2;
+        }
+        for(int i=1;i<n-1;i++){//right edge
+            dAdpi.setZero();
+
+            dAdpi.insert(i*n+n-1,(i)*n+n-1) = 3;
+            dAdpi.insert(i*n+n-1,(i-1)*n+n-1) = -1;
+            dAdpi.insert(i*n+n-1,(i+1)*n+n-1) = -1;
+            dAdpi.insert(i*n+n-1,(i)*n+n-2) = -1;
+
+            dAdpi.insert((i-1)*n+n-1,(i)*n+n-1) = 1;
+            dAdpi.insert((i+1)*n+n-1,(i)*n+n-1) = 1;
+            dAdpi.insert(i*n+n-1,(i)*n+n-1) = 1;
+            RowVectorXd dydpi = d * A_inv;
+            double test2 = dydpi * dAdpi * x;
+            dydp[i*n+n-1] = test2;
+        }
+        for(int i=1;i<n-1;i++){//bottom edge
+            dAdpi.setZero();
+
+            dAdpi.insert(i,(i)) = 3;
+            dAdpi.insert(i,(i-1)) = -1;
+            dAdpi.insert(i,(i+1)) = -1;
+            dAdpi.insert(i,i+n) = -1;
+
+            dAdpi.insert(i-1,(i)) = 1;
+            dAdpi.insert(i+1,(i)) = 1;
+            dAdpi.insert(i+n,(i)) = 1;
+            RowVectorXd dydpi = d * A_inv;
+            double test2 = dydpi * dAdpi * x;
+            dydp[i] = test2;
+        }
+        for(int i=1;i<n-1;i++){//top edge
+            dAdpi.setZero();
+
+            dAdpi.insert(n*(n-1)+i,n*(n-1)+i) = 3;
+            dAdpi.insert(n*(n-1)+i,n*(n-1)+i+1) = -1;
+            dAdpi.insert(n*(n-1)+i,n*(n-1)+i-1) = -1;
+            dAdpi.insert(n*(n-1)+i,n*(n-2)+i) = -1;
+
+            dAdpi.insert(n*(n-1)+i+1,n*(n-1)+i) = 1;
+            dAdpi.insert(n*(n-1)+i-1,n*(n-1)+i) = 1;
+            dAdpi.insert(n*(n-2)+i,n*(n-1)+i) = 1;
+            RowVectorXd dydpi = d * A_inv;
+            double test2 = dydpi * dAdpi * x;
+            dydp[i*n] = test2;
+        }
     }
 };
 
