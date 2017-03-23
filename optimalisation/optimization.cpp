85cec07d6d51410974a8aec84a79f0177c870da8
NeleN <nele.nauwelaers@hotmail.be>
2017-03-23 15:26:07 +0100

    Add files via upload

--- optimalisation/optimization.cpp
+++ optimalisation/optimization.cpp
@@ -34,7 +34,7 @@ bool HS071_NLP::get_nlp_info(Index& n, Index& m, Index& nnz_jac_g,
   m = 1;
 
   // in this example the jacobian is dense and contains 8 nonzeros
-  nnz_jac_g = 8;
+  nnz_jac_g = N*N;
 
   // the hessian is also dense and has 16 total nonzeros, but we
   // only need the lower left corner (since it is symmetric)
@@ -99,10 +99,10 @@ bool HS071_NLP::get_starting_point(Index n, bool init_x, Number* x,
 		x[i] = 0.4;
 	}
 
-																															//  x[0] = 3.0;
-																															//  x[1] = 5.0;
-																															//  x[2] = 2.0;
-																															//  x[3] = 1.0;
+										//  x[0] = 3.0;
+										//  x[1] = 5.0;
+										//  x[2] = 2.0;
+										//  x[3] = 1.0;
 
   return true;
 }
@@ -113,7 +113,9 @@ bool HS071_NLP::eval_f(Index n, const Number* x, bool new_x, Number& obj_value)
   assert(n == N*N);
 	SparseMatrix<double>A(N*N,N*N)
 	double solution[N*N];
-	solve_heath(reinterpret_cast<int(*)[N][N]>(x),solution)
+	double k[N*N];
+	k = x*80 + (1 - x)*0.01;
+	solve_heath(reinterpret_cast<int(*)[N][N]>(k),solution)
 
 	for (int i=0;i<N*N;i++){
 		assert(solution[i]>=300);
@@ -122,7 +124,7 @@ bool HS071_NLP::eval_f(Index n, const Number* x, bool new_x, Number& obj_value)
 
 	obj_value = obj_value/(N*N);
 
-																															// obj_value = x[0] * x[3] * (x[0] + x[1] + x[2]) + x[2];
+										// obj_value = x[0] * x[3] * (x[0] + x[1] + x[2]) + x[2];
 
   return true;
 }
@@ -134,10 +136,10 @@ bool HS071_NLP::eval_grad_f(Index n, const Number* x, bool new_x, Number* grad_f
 
 // TODO: Adjoint
 
-																															//  grad_f[0] = x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]);
-																															//  grad_f[1] = x[0] * x[3];
-																															//  grad_f[2] = x[0] * x[3] + 1;
-																															//  grad_f[3] = x[0] * (x[0] + x[1] + x[2]);
+										//  grad_f[0] = x[0] * x[3] + x[3] * (x[0] + x[1] + x[2]);
+										//  grad_f[1] = x[0] * x[3];
+										//  grad_f[2] = x[0] * x[3] + 1;
+										//  grad_f[3] = x[0] * (x[0] + x[1] + x[2]);
 
   return true;
 }
@@ -153,8 +155,8 @@ bool HS071_NLP::eval_g(Index n, const Number* x, bool new_x, Index m, Number* g)
 	}
 	g[0] = g[0]/(N*N); 
 
-																															//  g[0] = x[0] * x[1] * x[2] * x[3];
-																															//  g[1] = x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
+										//  g[0] = x[0] * x[1] * x[2] * x[3];
+										//  g[1] = x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
 
   return true;
 }
@@ -168,40 +170,38 @@ bool HS071_NLP::eval_jac_g(Index n, const Number* x, bool new_x,
 		values[i] = 1/(N*N);
 	}
 	
-																														  // if (values == NULL) {
-																														  //  return the structure of the jacobian
+										 // if (values == NULL) {
+										 //  return the structure of the jacobian
 
-																														  //  this particular jacobian is dense
-																															// iRow[0] = 0;
-																															// jCol[0] = 0;
-																															// iRow[1] = 0;
-																															// jCol[1] = 1;
-																															// iRow[2] = 0;
-																															// jCol[2] = 2;
-																															// iRow[3] = 0;
-																															// jCol[3] = 3;
-																															// iRow[4] = 1;
-																															// jCol[4] = 0;
-																															// iRow[5] = 1;
-																															// jCol[5] = 1;
-																															// iRow[6] = 1;
-																															// jCol[6] = 2;
-																															// iRow[7] = 1;
-																															// jCol[7] = 3;
-																														  // }
-																														  // else {
-																														  //  return the values of the jacobian of the constraints
-
-																															// values[0] = x[1]*x[2]*x[3]; // 0,0
-																															// values[1] = x[0]*x[2]*x[3]; // 0,1
-																															// values[2] = x[0]*x[1]*x[3]; // 0,2
-																															// values[3] = x[0]*x[1]*x[2]; // 0,3
-
-																															// values[4] = 2*x[0]; // 1,0
-																															// values[5] = 2*x[1]; // 1,1
-																															// values[6] = 2*x[2]; // 1,2
-																															// values[7] = 2*x[3]; // 1,3
-																														  // }
+											//  this particular jacobian is dense
+											// iRow[0] = 0;
+											// jCol[0] = 0;
+											// iRow[1] = 0;
+											// jCol[1] = 1;
+											// iRow[2] = 0;
+											// jCol[2] = 2;
+											// iRow[3] = 0;
+											// jCol[3] = 3;
+											// iRow[4] = 1;
+											// jCol[4] = 0;
+											// iRow[5] = 1;
+											// jCol[5] = 1;
+											// iRow[6] = 1;
+											// jCol[6] = 2;
+											// iRow[7] = 1;
+											// jCol[7] = 3;
+										  // }
+										  // else {
+										  //  return the values of the jacobian of the constraints
+											// values[0] = x[1]*x[2]*x[3]; // 0,0
+											// values[1] = x[0]*x[2]*x[3]; // 0,1
+											// values[2] = x[0]*x[1]*x[3]; // 0,2
+											// values[3] = x[0]*x[1]*x[2]; // 0,3
+											// values[4] = 2*x[0]; // 1,0
+											// values[5] = 2*x[1]; // 1,1
+											// values[6] = 2*x[2]; // 1,2
+											// values[7] = 2*x[3]; // 1,3
+											// }
 
   return true;
 }
@@ -212,60 +212,7 @@ bool HS071_NLP::eval_h(Index n, const Number* x, bool new_x,
                        bool new_lambda, Index nele_hess, Index* iRow,
                        Index* jCol, Number* values)
 {
-  if (values == NULL) {
-    // return the structure. This is a symmetric matrix, fill the lower left
-    // triangle only.
-
-    // the hessian for this problem is actually dense
-    Index idx=0;
-    for (Index row = 0; row < 4; row++) {
-      for (Index col = 0; col <= row; col++) {
-        iRow[idx] = row;
-        jCol[idx] = col;
-        idx++;
-      }
-    }
-
-    assert(idx == nele_hess);
-  }
-  else {
-    // return the values. This is a symmetric matrix, fill the lower left
-    // triangle only
-
-    // fill the objective portion
-    values[0] = obj_factor * (2*x[3]); // 0,0
-
-    values[1] = obj_factor * (x[3]);   // 1,0
-    values[2] = 0.;                    // 1,1
-
-    values[3] = obj_factor * (x[3]);   // 2,0
-    values[4] = 0.;                    // 2,1
-    values[5] = 0.;                    // 2,2
-
-    values[6] = obj_factor * (2*x[0] + x[1] + x[2]); // 3,0
-    values[7] = obj_factor * (x[0]);                 // 3,1
-    values[8] = obj_factor * (x[0]);                 // 3,2
-    values[9] = 0.;                                  // 3,3
-
-
-    // add the portion for the first constraint
-    values[1] += lambda[0] * (x[2] * x[3]); // 1,0
-
-    values[3] += lambda[0] * (x[1] * x[3]); // 2,0
-    values[4] += lambda[0] * (x[0] * x[3]); // 2,1
-
-    values[6] += lambda[0] * (x[1] * x[2]); // 3,0
-    values[7] += lambda[0] * (x[0] * x[2]); // 3,1
-    values[8] += lambda[0] * (x[0] * x[1]); // 3,2
-
-    // add the portion for the second constraint
-    values[0] += lambda[1] * 2; // 0,0
-
-    values[2] += lambda[1] * 2; // 1,1
-
-    values[5] += lambda[1] * 2; // 2,2
-
-    values[9] += lambda[1] * 2; // 3,3
+	values[0] = 0;
   }
 
   return true;
